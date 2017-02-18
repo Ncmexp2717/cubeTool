e958702ca7492f3444dec036b1246cdd9be05f53
Naoya Yamaguchi <yamaguchi.cphys@gmail.com>
2017-02-18 16:48:32 +0900

    Delete argc_3-2.h

--- cubeTool/argc_3-2.h
+++
@@ -1,9 +0,0 @@
-evalTcl(interp, "argc_3-2", HEREDOC(
-seek $fp1 $fp1Offset;
-seek $fp2 $fp2Offset;
-if {[gets $fp1 data1]>=0 || [gets $fp2 data2]>=0} {
-  puts stderr "Error: Wrong line.";
-  exit 2;
-};
-)
-);
